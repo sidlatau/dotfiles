3deb2a34a7051dd578b146f56d5e69ff83d24a95
Tautvydas <tautvydas.sidlauskas@gmail.com>
2022-01-23 20:03:48 +0200

    Add lazygit git config to dotfiles

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 stow --target=${HOME} nvim --ignore=.DS_Store 
 stow --target=${HOME} zsh --ignore=.DS_Store 
 stow --target=${HOME} lvim --ignore=.DS_Store 
+stow --target=${HOME} lazygit --ignore=.DS_Store 
