68902906cf958d02fb4060780f7423f116fb8308
Tautvydas <tautvydas.sidlauskas@gmail.com>
2022-01-23 17:54:35 +0200

    Add built value snippets

--- nvim/.config/nvim/lua/user/cmp.lua
+++ nvim/.config/nvim/lua/user/cmp.lua
@@ -8,7 +8,10 @@ if not snip_status_ok then
   return
 end
 
-require("luasnip/loaders/from_vscode").lazy_load()
+require("luasnip/loaders/from_vscode").lazy_load {
+  paths = { "~/Documents/github/personal/dotfiles/snippets" },
+}
+require("luasnip").filetype_extend("dart", { "flutter" })
 
 local check_backspace = function()
   local col = vim.fn.col "." - 1
