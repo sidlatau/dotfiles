4e3309421ea2dd6b2e1af60c17d7ded5e8e15ed0
Tautvydas <tautvydas.sidlauskas@gmail.com>
2022-02-16 21:59:05 +0200

    Remove not used snippets

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -77,7 +77,6 @@ return packer.startup(function(use)
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
-  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
