8bb597b3f7476eb441ee13dc7c4f6dfaa1df7038
Tautvydas <tautvydas.sidlauskas@gmail.com>
2022-02-19 11:46:00 +0200

    Stow kitty config

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ stow --target=${HOME} zsh --ignore=.DS_Store
 stow --target=${HOME} lvim --ignore=.DS_Store 
 stow --target=${HOME} lazygit --ignore=.DS_Store 
 stow --target=${HOME} git --ignore=.DS_Store 
+stow --target=${HOME} kitty --ignore=.DS_Store 
