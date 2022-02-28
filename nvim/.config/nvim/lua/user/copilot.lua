e7f95739dcff5b121fb92904240e96753164287e
Tautvydas <tautvydas.sidlauskas@gmail.com>
2022-02-28 11:18:59 +0200

    Disable copilot - too much noise

--- nvim/.config/nvim/lua/user/copilot.lua
+++ nvim/.config/nvim/lua/user/copilot.lua
@@ -1,11 +1,8 @@
--- vim.g.copilot_no_tab_map = true
--- vim.g.copilot_assume_mapped = true
--- vim.g.copilot_tab_fallback = ""
 vim.g.copilot_filetypes = {
   ["*"] = false,
   gitcommit = false,
   NeogitCommitMessage = false,
-  dart = true,
+  dart = false,
   lua = true,
 }
 vim.api.nvim_set_keymap(
