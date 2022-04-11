f16a480a9c35ef86887df37fdf8dbb15ec12ea11
Tautvydas <tautvydas.sidlauskas@gmail.com>
2022-04-11 08:29:06 +0300

    Disable bold

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,7 +1,7 @@
 vim.cmd [[
 let g:gruvbox_material_background = 'hard'
 let g:gruvbox_material_better_performance = 1
-let g:gruvbox_material_enable_bold = 1
+let g:gruvbox_material_enable_bold = 0
 ]]
 local colorscheme = "gruvbox-material"
 
