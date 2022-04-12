9be4b4b8ecae80e2682888f5d748cdcfb41bcb15
Tautvydas <tautvydas.sidlauskas@gmail.com>
2022-04-12 21:34:56 +0300

    Simplify notify config

--- nvim/.config/nvim/lua/user/notify.lua
+++ nvim/.config/nvim/lua/user/notify.lua
@@ -3,19 +3,11 @@ if not status_ok then
   return
 end
 
----@type table<string, fun(bufnr: number, notif: table, highlights: table)>
-local renderer = require "notify.render"
 notify.setup {
   stages = "fade_in_slide_out",
   timeout = 3000,
   minimum_width = 40,
   background_colour = "#282828",
-  render = function(bufnr, notif, highlights)
-    if notif.title[1] == "" then
-      return renderer.minimal(bufnr, notif, highlights)
-    end
-    return renderer.default(bufnr, notif, highlights)
-  end,
 }
 vim.notify = notify
 require("telescope").load_extension "notify"
