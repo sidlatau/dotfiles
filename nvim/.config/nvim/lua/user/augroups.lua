5ce10d0ca426fde666a513d5946e1f4f0632b16d
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2022-04-17 19:22:00 +0300

    Rewrite augroups to use lua api

--- nvim/.config/nvim/lua/user/augroups.lua
+++ nvim/.config/nvim/lua/user/augroups.lua
@@ -1,70 +1,34 @@
-local M = {}
---- Create autocommand groups based on the passed definitions
----@param definitions table contains trigger, pattern and text. The key will be used as a group name
----@param buffer boolean indicate if the augroup should be local to the buffer
-function M.define_augroups(definitions, buffer)
-  for group_name, definition in pairs(definitions) do
-    vim.cmd("augroup " .. group_name)
-    if buffer then
-      vim.cmd [[autocmd! * <buffer>]]
-    else
-      vim.cmd [[autocmd!]]
-    end
+local format_on_save = vim.api.nvim_create_augroup("format_on_save", {})
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = format_on_save,
+  pattern = "*",
+  callback = function()
+    vim.lsp.buf.formatting_sync()
+  end,
+})
 
-    for _, def in pairs(definition) do
-      local command = table.concat(vim.tbl_flatten { "autocmd", def }, " ")
-      vim.cmd(command)
-    end
+local fix_all_on_save = vim.api.nvim_create_augroup("fix_all_on_save", {})
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = fix_all_on_save,
+  pattern = "*.dart",
+  callback = function()
+    require("user.lsp.handlers").code_action_fix_all()
+  end,
+})
 
-    vim.cmd "augroup END"
-  end
-end
-
-function M.enable_format_on_save(opts)
-  local fmd_cmd = string.format("LspFormat", opts.timeout)
-  M.define_augroups {
-    format_on_save = { { "BufWritePre", opts.pattern, fmd_cmd } },
-  }
-end
-
-function M.enable_fix_all_on_save()
-  local fmd_cmd = ":LspFixAll"
-
-  M.define_augroups {
-    fix_all_on_save = { { "BufWritePre", "*.dart", fmd_cmd } },
-  }
-end
-
-M.enable_format_on_save { pattern = "*", timeout = 200 }
-M.enable_fix_all_on_save()
-
-vim.api.nvim_create_user_command("LspFormat", function()
-  vim.lsp.buf.formatting_sync()
-end, {})
-
-vim.api.nvim_create_user_command("LspFixAll", function()
-  require("user.lsp.handlers").code_action_fix_all()
-end, {})
-
-vim.cmd [[
-  augroup _general_settings
-    autocmd!
-    " Simplify exit
-    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
-
-    " Treat arb files as json
-    autocmd BufRead *.arb :set filetype=json
-  augroup end
-]]
-
-vim.cmd [[
-  augroup _git
-    autocmd!
-    autocmd FileType gitcommit setlocal spell
-  augroup end
-
-  augroup _markdown
-    autocmd!
-    autocmd FileType markdown setlocal spell
-  augroup end
-]]
+local general_settings = vim.api.nvim_create_augroup("general_settings", {})
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = general_settings,
+  pattern = "qf,help,man,lspinfo",
+  command = "nnoremap <silent> <buffer> q :close<CR> ",
+})
+vim.api.nvim_create_autocmd({ "BufRead" }, {
+  group = general_settings,
+  pattern = "*.arb",
+  command = ":set filetype=json",
+})
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = general_settings,
+  pattern = "gitcommit,markdown",
+  command = "setlocal spell",
+})
