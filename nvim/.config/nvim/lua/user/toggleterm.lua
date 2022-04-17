cdaca5a1902485a62727565683ef7f45f20e0a7b
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2022-04-17 20:40:04 +0300

    Remove not used lazygit mapping

--- nvim/.config/nvim/lua/user/toggleterm.lua
+++ nvim/.config/nvim/lua/user/toggleterm.lua
@@ -37,10 +37,3 @@ function _G.set_terminal_keymaps()
 end
 
 vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
-
-local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new { cmd = "lazygit", hidden = true }
-
-function _LAZYGIT_TOGGLE()
-  lazygit:toggle()
-end
