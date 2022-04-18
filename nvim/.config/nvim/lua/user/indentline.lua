119d17769eebf6bd92658c7b375553698a2cfcb1
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2022-04-18 12:47:48 +0300

    Make intendline not so intense

--- nvim/.config/nvim/lua/user/indentline.lua
+++ nvim/.config/nvim/lua/user/indentline.lua
@@ -1,8 +1,7 @@
 return function()
   require("indent_blankline").setup {
-    char = "│", -- ┆ ┊ 
+    char = "", -- ┆ ┊
     show_foldtext = false,
-    context_char = "┃",
     show_current_context = true,
     show_current_context_start = false,
     show_current_context_start_on_current_line = false,
