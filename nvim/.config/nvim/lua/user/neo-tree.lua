888a2c7259f0c497d75beea7c7b06a1bfd80f78a
Tautvydas <tautvydas.sidlauskas@gmail.com>
2022-05-02 22:27:39 +0300

    feat(vim): update neo-tree

--- nvim/.config/nvim/lua/user/neo-tree.lua
+++ nvim/.config/nvim/lua/user/neo-tree.lua
@@ -20,23 +20,31 @@ neo_tree.setup {
     indent = {
       indent_size = 2,
       padding = 1, -- extra padding on left hand side
+      -- indent guides
       with_markers = true,
       indent_marker = "│",
       last_indent_marker = "└",
       highlight = "NeoTreeIndentMarker",
+      -- expander config, needed for nesting files
+      with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
+      expander_collapsed = "",
+      expander_expanded = "",
+      expander_highlight = "NeoTreeExpander",
     },
     icon = {
-      folder_closed = "",
-      folder_open = "",
+      folder_closed = "",
+      folder_open = "",
       folder_empty = "ﰊ",
-      default = "*",
     },
     name = {
       trailing_slash = false,
       use_git_status_colors = true,
     },
     git_status = {
-      highlight = "NeoTreeDimText", -- if you remove this the status will be colorful
+      symbols = {
+        added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+        modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+      },
     },
   },
   filesystem = {
