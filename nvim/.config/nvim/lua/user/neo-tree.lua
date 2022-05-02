b9e4a3828bffd392f9bc3c224fb5da35ddb37b8b
Tautvydas <tautvydas.sidlauskas@gmail.com>
2022-05-02 22:30:27 +0300

    feat(vim): use h for closing node

--- nvim/.config/nvim/lua/user/neo-tree.lua
+++ nvim/.config/nvim/lua/user/neo-tree.lua
@@ -70,7 +70,7 @@ neo_tree.setup {
         ["<cr>"] = "open",
         ["S"] = "open_split",
         ["s"] = "open_vsplit",
-        ["C"] = "close_node",
+        ["h"] = "close_node",
         ["<bs>"] = "navigate_up",
         ["."] = "set_root",
         ["H"] = "toggle_hidden",
