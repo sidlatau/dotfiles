17e30f0855e1963bc4fe10121ee682c070eb9be6
Tautvydas <tautvydas.sidlauskas@gmail.com>
2022-05-11 10:40:32 +0300

    feat(vim): add tsserver to LSP

--- nvim/.config/nvim/lua/user/lsp/configs.lua
+++ nvim/.config/nvim/lua/user/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require "lspconfig"
 
-local servers = { "jsonls", "sumneko_lua", "yamlls" }
+local servers = { "jsonls", "sumneko_lua", "yamlls", "tsserver" }
 
 lsp_installer.setup {
   ensure_installed = servers,
