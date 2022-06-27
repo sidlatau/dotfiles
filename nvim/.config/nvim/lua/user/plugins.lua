8be9401d472a0ff252dfabcba888b6233ea03fde
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2022-06-27 19:13:09 +0300

    feat(vim): add lua types

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -222,6 +222,8 @@ return packer.startup(function(use)
     end,
   }
   use "wsdjeg/vim-fetch"
+  use "nvim-treesitter/playground"
+  use "folke/lua-dev.nvim"
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
