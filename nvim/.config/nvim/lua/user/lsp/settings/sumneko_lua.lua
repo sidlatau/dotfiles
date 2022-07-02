547dcf8ea36fad0084c256755f87242cade65773
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2022-07-02 21:54:34 +0300

    fix(vim): let stylua format lua code

--- nvim/.config/nvim/lua/user/lsp/settings/sumneko_lua.lua
+++ nvim/.config/nvim/lua/user/lsp/settings/sumneko_lua.lua
@@ -4,6 +4,9 @@ return {
       diagnostics = {
         globals = { "vim", "lvim" },
       },
+      format = {
+        enable = false,
+      },
       IntelliSense = {
         traceLocalSet = true,
       },
