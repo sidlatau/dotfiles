a2974e96078bd7f13e1f30249686390df53260af
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2022-07-03 10:05:56 +0300

    feat(vim): configure test command in neotest-dart adapter

--- nvim/.config/nvim/lua/user/test.lua
+++ nvim/.config/nvim/lua/user/test.lua
@@ -3,7 +3,7 @@ vim.g["test#dart#fluttertest#executable"] = "fvm flutter test"
 require("neotest").setup {
   adapters = {
     require "neotest-dart" {
-      fvm = true,
+      command = "fvm flutter",
     },
     require "neotest-vim-test" {
       ignore_filetypes = { "python", "lua", "dart" },
