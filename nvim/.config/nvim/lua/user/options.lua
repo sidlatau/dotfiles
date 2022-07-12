84d35e837f5b8f74c64ac5abfc2c7375670aacde
Tautvydas <tautvydas.sidlauskas@gmail.com>
2022-07-12 08:04:26 +0300

    fix: remove option to not load filetypes

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -1,5 +1,4 @@
 vim.g.do_filetype_lua = 1
-vim.g.did_load_filetypes = 0
 
 vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
 vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
