cda1a8fdc0a1558bc1e2ed9ee9dbb071583bea1f
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2022-07-28 23:16:53 +0300

    feat(vim): make package files not modifiable

--- nvim/.config/nvim/lua/user/autocommands.lua
+++ nvim/.config/nvim/lua/user/autocommands.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: missing-parameter
 local fix_all_on_save = vim.api.nvim_create_augroup("fix_all_on_save", {})
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   group = fix_all_on_save,
@@ -27,3 +28,15 @@ vim.api.nvim_create_autocmd(
     end,
   }
 )
+
+local dart = vim.api.nvim_create_augroup "dart"
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = dart,
+  pattern = "dart",
+  callback = function()
+    local full_path = vim.fn.expand "%:p"
+    if string.find(full_path, "/fvm/versions/") then
+      vim.api.nvim_buf_set_option(0, "modifiable", false)
+    end
+  end,
+})
