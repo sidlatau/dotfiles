e373ac6006fdb00ddda2e82633e2ed5515868423
Tautvydas <tautvydas.sidlauskas@gmail.com>
2022-08-08 16:18:49 +0300

    fix(colorscheme): remove italics

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -2,7 +2,6 @@ vim.g.gruvbox_material_background = "hard"
 vim.g.gruvbox_material_better_performance = 1
 vim.g.gruvbox_material_enable_bold = 0
 -- vim.g.gruvbox_material_foreground = "original"
-vim.g.gruvbox_material_enable_italic = 1
 vim.g.gruvbox_material_visual = "green background"
 vim.g.gruvbox_material_show_eob = 0
 vim.g.gruvbox_material_diagnostic_text_highlight = 1
