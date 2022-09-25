f93b6b80a8f991518277225f3afd004a37c1a821
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2022-09-25 20:16:13 +0300

    feat(vim): add markdown previewer

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -229,6 +229,14 @@ return packer.startup {
         }
       end,
     }
+    use {
+      "iamcco/markdown-preview.nvim",
+      run = "cd app && npm install",
+      setup = function()
+        vim.g.mkdp_filetypes = { "markdown" }
+      end,
+      ft = { "markdown" },
+    }
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
