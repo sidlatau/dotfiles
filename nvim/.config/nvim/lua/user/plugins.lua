1a7f7693b7e2279bb2c469c19e9c574e3ba9147e
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2022-10-01 17:38:36 +0300

    vim: add same text highligh

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -237,6 +237,7 @@ return packer.startup {
       end,
       ft = { "markdown" },
     }
+    use { "RRethy/vim-illuminate" }
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
