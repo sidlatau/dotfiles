1d5a09e277a731f2e78d045fe91d0f8beafb88c6
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2022-10-04 22:29:09 +0300

    delaytrain: disable arrow keys only

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -242,7 +242,14 @@ return packer.startup {
     use {
       "ja-ford/delaytrain.nvim",
       config = function()
-        require("delaytrain").setup()
+        require("delaytrain").setup {
+          delay_ms = 1000, -- How long repeated usage of a key should be prevented
+          grace_period = 1, -- How many repeated keypresses are allowed
+          keys = { -- Which keys (in which modes) should be delayed
+            -- ["nv"] = { "h", "j", "k", "l" },
+            ["nvi"] = { "<Left>", "<Down>", "<Up>", "<Right>" },
+          },
+        }
       end,
     }
     -- Automatically set up your configuration after cloning packer.nvim
