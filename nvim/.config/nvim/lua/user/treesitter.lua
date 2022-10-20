55db9f2a15e63bd58c0af9111719afaf6924043d
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2022-10-20 21:42:31 +0300

    vim: disable TS highligh
    For some reason when disabled dart methods are highlighted

--- nvim/.config/nvim/lua/user/treesitter.lua
+++ nvim/.config/nvim/lua/user/treesitter.lua
@@ -3,9 +3,6 @@ configs.setup {
   ensure_installed = { "lua", "dart" },
   sync_install = false,
   ignore_install = { "" }, -- List of parsers to ignore installing
-  highlight = {
-    enable = true,
-  },
   textobjects = {
     select = {
       enable = true,
