c5970636ff6b7367a14352a853e4dcca2f053e2b
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2022-10-22 15:10:00 +0300

    vim: update floating popups color

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,7 +1,6 @@
 vim.g.gruvbox_material_background = "hard"
 vim.g.gruvbox_material_better_performance = 1
 vim.g.gruvbox_material_enable_bold = 0
--- vim.g.gruvbox_material_foreground = "original"
 vim.g.gruvbox_material_visual = "green background"
 vim.g.gruvbox_material_show_eob = 0
 vim.g.gruvbox_material_diagnostic_text_highlight = 1
@@ -9,6 +8,14 @@ vim.g.gruvbox_material_diagnostic_virtual_text = 1
 vim.g.gruvbox_material_enable_italic = 1
 
 local colorscheme = "gruvbox-material"
+local grpid =
+  vim.api.nvim_create_augroup("custom_highlights_gruvboxmaterial", {})
+vim.api.nvim_create_autocmd("ColorScheme", {
+  group = grpid,
+  pattern = "gruvbox-material",
+  -- floating popups
+  command = "hi NormalFloat guibg=#282828 |" .. "hi FloatBorder guibg=#282828",
+})
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
