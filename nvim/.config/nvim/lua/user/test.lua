46b8e0cfc8441d39f9ce2b8e512742161a8c347f
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2022-10-22 13:45:18 +0300

    feat: add more setup for neo-test

--- nvim/.config/nvim/lua/user/test.lua
+++ nvim/.config/nvim/lua/user/test.lua
@@ -1,9 +1,40 @@
-require("neotest").setup {
+local status_ok, neotest = pcall(require, "neotest")
+if not status_ok then
+  return
+end
+
+local namespace = vim.api.nvim_create_namespace "neotest"
+vim.diagnostic.config({
+  virtual_text = {
+    format = function(diagnostic)
+      vim.pretty_print(diagnostic)
+      return diagnostic.message
+        :gsub("\n", " ")
+        :gsub("\t", " ")
+        :gsub("%s+", " ")
+        :gsub("^%s+", "")
+    end,
+  },
+}, namespace)
+
+neotest.setup {
   adapters = {
     require "neotest-dart" {
       command = "fvm flutter",
     },
     require "neotest-plenary",
+  },
+  discovery = {
+    enabled = false,
+  },
+  diagnostic = {
+    enabled = true,
+  },
+  floating = {
+    border = "rounded",
+    max_height = 0.8,
+    max_width = 0.8,
+    options = {},
   },
   icons = {
     expanded = "",
@@ -19,3 +50,13 @@ require("neotest").setup {
     unknown = "",
   },
 }
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set("n", "]n", function()
+  neotest.jump.next { status = "failed" }
+end, opts)
+vim.keymap.set("n", "[n", function()
+  neotest.jump.prev { status = "failed" }
+end, opts)
+vim.keymap.set("n", "]t", neotest.jump.next, opts)
+vim.keymap.set("n", "[t", neotest.jump.prev, opts)
