8d3d864f1607ce281ead9395929436388c62d591
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2022-12-30 09:26:39 +0200

    vim: show preview of images

--- nvim/.config/nvim/lua/user/telescope_config.lua
+++ nvim/.config/nvim/lua/user/telescope_config.lua
@@ -10,11 +10,43 @@ local actions = require "telescope.actions"
 
 telescope.setup {
   defaults = {
-
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" },
     file_ignore_patterns = { "%.g.dart", "%.freezed.dart" },
+    preview = {
+      mime_hook = function(filepath, bufnr, opts)
+        local is_image = function(filepath)
+          local image_extensions = { "png", "jpg", "jpeg", "gif" } -- Supported image formats
+          local split_path = vim.split(filepath:lower(), ".", { plain = true })
+          local extension = split_path[#split_path]
+          return vim.tbl_contains(image_extensions, extension)
+        end
+        if is_image(filepath) then
+          local term = vim.api.nvim_open_term(bufnr, {})
+          local function send_output(_, data, _)
+            for _, d in ipairs(data) do
+              vim.api.nvim_chan_send(term, d .. "\r\n")
+            end
+          end
+
+          vim.fn.jobstart({
+            "viu",
+            "-b",
+            filepath,
+          }, {
+            on_stdout = send_output,
+            stdout_buffered = true,
+          })
+        else
+          require("telescope.previewers.utils").set_preview_message(
+            bufnr,
+            opts.winid,
+            "Binary cannot be previewed"
+          )
+        end
+      end,
+    },
   },
   pickers = {
     -- Default configuration for builtin pickers goes here:
