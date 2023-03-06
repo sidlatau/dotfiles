173580812193f29a703450867833788e6402b86c
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2023-03-06 22:50:45 +0200

    go: remove not used anymore mapping

--- nvim/.config/nvim/after/ftplugin/go.lua
+++ nvim/.config/nvim/after/ftplugin/go.lua
@@ -1,16 +1,1 @@
 vim.keymap.set("n", "<leader>rr", "<cmd>w<cr><cmd>GoRun<cr>", { buffer = 0 })
-
-vim.keymap.set("n", "<leader>q", function()
-  local qf_exists = false
-  for _, win in pairs(vim.fn.getwininfo()) do
-    if win["quickfix"] == 1 then
-      qf_exists = true
-    end
-  end
-  print(qf_exists)
-  if qf_exists == true then
-    vim.cmd "cclose"
-    return
-  end
-  vim.cmd "copen"
-end, { buffer = 0 })
