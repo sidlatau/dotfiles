93d898c433e969bb40ba238b6c5044c90a6ba200
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2023-03-23 13:18:40 +0200

    copilot: do not run copilot in some file types

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,13 @@
 return {
   "github/copilot.vim",
+  config = function()
+    vim.g.copilot_filetypes = {
+      ["*"] = true,
+      gitcommit = false,
+      DressingInput = false,
+      TelescopePrompt = false,
+      ["neo-tree-popup"] = false,
+      ["dap-repl"] = false,
+    }
+  end,
 }
