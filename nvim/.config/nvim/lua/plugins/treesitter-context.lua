d24dc763387e3bd3487542161c4f1dc52cfa832d
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2023-03-24 20:07:30 +0200

    vim: show more context

--- nvim/.config/nvim/lua/plugins/treesitter-context.lua
+++ nvim/.config/nvim/lua/plugins/treesitter-context.lua
@@ -1,8 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter-context",
   config = function()
-    require("treesitter-context").setup {
-      max_lines = 1, -- How many lines the window should span. Values <= 0 mean no limit.
-    }
+    require("treesitter-context").setup {}
   end,
 }
