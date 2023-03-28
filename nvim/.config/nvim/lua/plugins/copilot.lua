d2e6a558cb07075d696848796541e031acc01e10
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2023-03-28 22:03:30 +0300

    copilot: add additional copilot mappings

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,6 +1,18 @@
 return {
   "github/copilot.vim",
   config = function()
+    local function accept_word()
+      vim.fn["copilot#Accept"] ""
+      local bar = vim.fn["copilot#TextQueuedForInsertion"]()
+      return vim.fn.split(bar, [[[ .]\zs]])[1]
+    end
+
+    local function accept_line()
+      vim.fn["copilot#Accept"] ""
+      local bar = vim.fn["copilot#TextQueuedForInsertion"]()
+      return vim.fn.split(bar, [[[\n]\zs]])[1]
+    end
+
     vim.g.copilot_filetypes = {
       ["*"] = true,
       gitcommit = false,
@@ -9,5 +21,24 @@ return {
       ["neo-tree-popup"] = false,
       ["dap-repl"] = false,
     }
+    vim.keymap.set(
+      "i",
+      "<C-/>",
+      "<Cmd>vertical Copilot panel<CR>",
+      { desc = "open copilot panel" }
+    )
+
+    vim.keymap.set(
+      "i",
+      "<M-w>",
+      accept_word,
+      { expr = true, remap = false, desc = "accept word" }
+    )
+    vim.keymap.set(
+      "i",
+      "<M-l>",
+      accept_line,
+      { expr = true, remap = false, desc = "accept line" }
+    )
   end,
 }
