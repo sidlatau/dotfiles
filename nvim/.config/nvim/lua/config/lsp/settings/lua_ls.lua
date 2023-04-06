14a89c38fc5d73747a6202b6efaa38a030d418ee
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2023-04-06 23:28:57 +0300

    lua_ls: update config

--- nvim/.config/nvim/lua/config/lsp/settings/lua_ls.lua
+++ nvim/.config/nvim/lua/config/lsp/settings/lua_ls.lua
@@ -4,24 +4,20 @@ table.insert(runtime_path, "lua/?/init.lua")
 return {
   settings = {
     Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = "LuaJIT",
-        -- Setup your lua path
-        path = runtime_path,
+      codeLens = { enable = true },
+      hint = {
+        enable = true,
+        arrayIndex = "Disable",
+        setType = true,
+        paramName = "Disable",
       },
+      format = { enable = false },
       diagnostics = {
-        globals = { "vim", "lvim" },
+        globals = { "vim" },
       },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false,
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
+      completion = { keywordSnippet = "Replace", callSnippet = "Replace" },
+      workspace = { checkThirdParty = false },
+      telemetry = { enable = false },
     },
   },
 }
