ff1f9570d6e24db792f562e98679342660451eb9
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2023-04-10 22:02:58 +0300

    treesitter-context: add separator

--- nvim/.config/nvim/lua/plugins/treesitter-context.lua
+++ nvim/.config/nvim/lua/plugins/treesitter-context.lua
@@ -2,6 +2,10 @@ return {
   "nvim-treesitter/nvim-treesitter-context",
   event = "VeryLazy",
   config = function()
-    require("treesitter-context").setup {}
+    require("treesitter-context").setup {
+      multiline_threshold = 4,
+      separator = { "─", "WinSeparator" }, -- alternatives: ▁ ─ ▄
+      mode = "cursor",
+    }
   end,
 }
