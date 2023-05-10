bbabb349fcdb27de6fc8a6907fb1b0f2440c4473
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2023-05-10 15:25:58 +0300

    signature: disable

--- nvim/.config/nvim/lua/plugins/lsp_signature.lua
+++ nvim/.config/nvim/lua/plugins/lsp_signature.lua
@@ -1,10 +1,12 @@
 return {
   "ray-x/lsp_signature.nvim",
   event = "VeryLazy",
+  enabled = false,
   config = function()
     local signature_config = {
       hint_enable = false,
       toggle_key = "<C-s>",
+      noice = true,
     }
     require("lsp_signature").setup(signature_config)
   end,
