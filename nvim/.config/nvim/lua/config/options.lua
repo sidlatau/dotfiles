7c2ece3cac78b943f747cf6d40706a24f4e41292
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2023-06-13 22:10:39 +0300

    vim: autosave

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -6,6 +6,7 @@ vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
 vim.opt.fileencoding = "utf-8" -- the encoding written to a file
 vim.opt.hlsearch = true -- highlight all matches on previous search pattern
 vim.opt.ignorecase = true -- ignore case in search patterns
+vim.opt.wrapscan = true -- Searches wrap around the end of the file
 vim.opt.mouse = "a" -- allow the mouse to be used in neovim
 vim.opt.pumheight = 10 -- pop up menu height
 vim.opt.showmode = false -- we don't need to see things like -- INSERT -- anymore
@@ -68,3 +69,4 @@ set fillchars+=diff:╱
 ]]
 vim.opt.report = 99999
 vim.opt.exrc = true
+vim.opt.autowriteall = true -- automatically :write before running commands and changing files
