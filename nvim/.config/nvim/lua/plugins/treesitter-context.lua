579af594bdd6474f6387f0207e8c1f7060f09d67
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2023-06-14 23:23:24 +0300

    vim: use single line for treesitter context

--- nvim/.config/nvim/lua/plugins/treesitter-context.lua
+++ nvim/.config/nvim/lua/plugins/treesitter-context.lua
@@ -2,10 +2,6 @@ return {
   "nvim-treesitter/nvim-treesitter-context",
   event = "VeryLazy",
   config = function()
-    require("treesitter-context").setup {
-      multiline_threshold = 4,
-      separator = { "─", "WinSeparator" }, -- alternatives: ▁ ─ ▄
-      mode = "cursor",
-    }
+    require("treesitter-context").setup {}
   end,
 }
