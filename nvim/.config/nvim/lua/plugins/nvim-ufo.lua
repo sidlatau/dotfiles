ac7c06caf5435952f3916ced9535b1d6d6c3dbb2
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2023-08-09 10:37:34 +0300

    fix: do not show fold level in status column

--- nvim/.config/nvim/lua/plugins/nvim-ufo.lua
+++ nvim/.config/nvim/lua/plugins/nvim-ufo.lua
@@ -1,38 +1,52 @@
 return {
-  "kevinhwang91/nvim-ufo",
-  event = "VeryLazy",
-  dependencies = { "kevinhwang91/promise-async" },
-  keys = {
-    {
-      "zR",
-      function()
-        require("ufo").openAllFolds()
-      end,
-      "open all folds",
+  -- UFO folding
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = {
+      "kevinhwang91/promise-async",
+      {
+        "luukvbaal/statuscol.nvim",
+        config = function()
+          local builtin = require "statuscol.builtin"
+          require("statuscol").setup {
+            relculright = true,
+            segments = {
+              { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+              { text = { "%s" }, click = "v:lua.ScSa" },
+              { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+            },
+          }
+        end,
+      },
     },
-    {
-      "zM",
-      function()
-        require("ufo").closeAllFolds()
+    event = "BufReadPost",
+    opts = {
+      provider_selector = function()
+        return { "treesitter", "indent" }
       end,
-      "close all folds",
     },
-    {
-      "zP",
-      function()
-        require("ufo").peekFoldedLinesUnderCursor()
-      end,
-      "preview fold",
+    keys = {
+      {
+        "zR",
+        function()
+          require("ufo").openAllFolds()
+        end,
+        "open all folds",
+      },
+      {
+        "zM",
+        function()
+          require("ufo").closeAllFolds()
+        end,
+        "close all folds",
+      },
+      {
+        "zP",
+        function()
+          require("ufo").peekFoldedLinesUnderCursor()
+        end,
+        "preview fold",
+      },
     },
   },
-  opts = function()
-    require("ufo").setup {
-      open_fold_hl_timeout = 0,
-      preview = {
-        win_config = { winhighlight = "Normal:Normal,FloatBorder:Normal" },
-      },
-      enable_get_fold_virt_text = true,
-      close_fold_kinds = { "imports", "comment" },
-    }
-  end,
 }
