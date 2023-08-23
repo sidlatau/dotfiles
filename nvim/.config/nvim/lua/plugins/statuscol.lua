cf7da1676c32596d60bb98472be3395a08b86083
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2023-08-23 22:25:04 +0300

    vim: remove ufo
    Looks like I am not using folds

---
+++ nvim/.config/nvim/lua/plugins/statuscol.lua
@@ -0,0 +1,14 @@
+return {
+  "luukvbaal/statuscol.nvim",
+  config = function()
+    local builtin = require "statuscol.builtin"
+    require("statuscol").setup {
+      relculright = true,
+      segments = {
+        { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+        { text = { "%s" }, click = "v:lua.ScSa" },
+        { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+      },
+    }
+  end,
+}
