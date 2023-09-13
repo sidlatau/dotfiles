c7bf0df92bba071aa477c2ee05e2cfe4563b8a5e
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2023-09-13 19:53:54 +0300

    auto-pairs: try again for dart

--- nvim/.config/nvim/lua/plugins/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/plugins/nvim-autopairs.lua
@@ -11,7 +11,7 @@ return {
         dart = { "string" },
         javascript = { "template_string" },
       },
-      disable_filetype = { "TelescopePrompt", "dart" },
+      disable_filetype = { "TelescopePrompt" },
     }
     -- autopairs.disable()
     -- credit: https://github.com/JoosepAlviste
