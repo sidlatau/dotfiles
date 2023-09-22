c7c43db9781b09b1f1422b52549ec508108357ec
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2023-09-22 08:21:56 +0300

    dial: add switching between findsOneWidget and findsNothing

--- nvim/.config/nvim/lua/plugins/dial.lua
+++ nvim/.config/nvim/lua/plugins/dial.lua
@@ -9,6 +9,11 @@ return {
         augend.integer.alias.decimal, -- nonnegative decimal number (0, 1, 2, 3, ...)
         augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
         augend.constant.alias.bool, -- boolean value (true <-> false)
+        augend.constant.new {
+          elements = { "findsOneWidget", "findsNothing" },
+          word = true,
+          cyclic = true,
+        },
       },
     }
   end,
