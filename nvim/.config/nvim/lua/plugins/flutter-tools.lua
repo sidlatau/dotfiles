7cb765248f8d8bc1b3a78c777ad44cd724140b5b
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2023-10-04 07:03:38 +0300

    flutter-tools: test dap changes

--- nvim/.config/nvim/lua/plugins/flutter-tools.lua
+++ nvim/.config/nvim/lua/plugins/flutter-tools.lua
@@ -1,5 +1,7 @@
 return {
-  "akinsho/flutter-tools.nvim",
+  -- "akinsho/flutter-tools.nvim",
+  "ch-vik/flutter-tools.nvim",
+  branch = "dap/flutter-run-project-path",
   -- dir = "~/Documents/github/personal/flutter-tools.nvim",
   dependencies = "nvim-lua/plenary.nvim",
   config = function()
@@ -37,6 +39,10 @@ return {
         run_via_dap = true,
         -- don't pause on exception in flutter
         exception_breakpoints = {},
+        register_configurations = function(paths)
+          require("dap").configurations.dart = {}
+          require("dap.ext.vscode").load_launchjs()
+        end,
       },
       fvm = true,
       widget_guides = {
