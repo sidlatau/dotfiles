e816f8c8714a794fa5431ea55c38857ccdcd08a2
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2023-10-18 23:36:16 +0300

    chatgpt: load token from pass

--- nvim/.config/nvim/lua/plugins/chatgpt.lua
+++ nvim/.config/nvim/lua/plugins/chatgpt.lua
@@ -3,6 +3,7 @@ return {
   event = "VeryLazy",
   config = function()
     require("chatgpt").setup {
+      api_key_cmd = "pass show api/tokens/openai",
       popup_input = {
         submit = "<CR>",
       },
