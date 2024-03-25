0e15b96e315e4c7aa0b2b87250d1437ae423bd09
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2024-03-25 20:18:45 +0200

    vim: update neotest

--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -1,9 +1,11 @@
 return {
   "nvim-neotest/neotest",
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
     "nvim-neotest/neotest-plenary",
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
   },
   event = "VeryLazy",
   config = function()
