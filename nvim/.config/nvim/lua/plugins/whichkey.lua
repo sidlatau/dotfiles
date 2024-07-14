e8aa81714e6a76a2719e177d27e5d83ffb553e17
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2024-07-14 17:38:44 +0300

    vim(which-key): make it not so intrusive

--- nvim/.config/nvim/lua/plugins/whichkey.lua
+++ nvim/.config/nvim/lua/plugins/whichkey.lua
@@ -4,6 +4,16 @@ return {
   opts = {
     preset = "modern",
     icons = { rules = false },
+    delay = function(ctx)
+      return 1500
+    end,
+    modes = {
+      x = false, -- Visual mode
+      s = false, -- Select mode
+      o = false, -- Operator pending mode
+      t = false, -- Terminal mode
+      c = false, -- Command mode
+    },
     plugins = {
       marks = false, -- shows a list of your marks on ' and `
       registers = false, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
@@ -20,7 +30,7 @@ return {
         windows = false, -- default bindings on <c-w>
         nav = false, -- misc bindings to work with windows
         z = true, -- bindings for folds, spelling and others prefixed with z
-        g = true, -- bindings for prefixed with g
+        g = false, -- bindings for prefixed with g
       },
     },
     spec = {
