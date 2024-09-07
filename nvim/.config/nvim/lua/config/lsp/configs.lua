b25b052e9e2f35d09af628beb5cc731ba6b756dd
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2024-09-07 23:55:51 +0300

    vim(typescript): fix lsp setup

--- nvim/.config/nvim/lua/config/lsp/configs.lua
+++ nvim/.config/nvim/lua/config/lsp/configs.lua
@@ -10,27 +10,34 @@ if not mason_status_ok then
 end
 local lspconfig = require "lspconfig"
 
-local servers = {
-  "jsonls",
-  "lua_ls",
-  "yamlls",
-  "eslint",
-  "ts_ls",
-}
-
 mason_lspconfig.setup {
-  ensure_installed = servers,
+  ensure_installed = {
+    "jsonls",
+    "lua_ls",
+    "yamlls",
+    "eslint",
+    "tsserver",
+  },
 }
-
-for _, server in pairs(servers) do
-  local opts = {
-    on_attach = require("config.lsp.handlers").on_attach,
-    capabilities = require("config.lsp.handlers").capabilities,
-  }
-  local has_custom_opts, server_custom_opts =
-    pcall(require, "config.lsp.settings." .. server)
-  if has_custom_opts then
-    opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
-  end
-  lspconfig[server].setup(opts)
-end
+-- automatically install ensure_installed servers
+require("mason-lspconfig").setup_handlers {
+  -- Will be called for each installed server that doesn't have
+  -- a dedicated handler.
+  --
+  function(server_name) -- default handler (optional)
+    -- https://github.com/neovim/nvim-lspconfig/pull/3232
+    if server_name == "tsserver" then
+      server_name = "ts_ls"
+    end
+    local opts = {
+      on_attach = require("config.lsp.handlers").on_attach,
+      capabilities = require("config.lsp.handlers").capabilities,
+    }
+    local has_custom_opts, server_custom_opts =
+      pcall(require, "config.lsp.settings." .. server_name)
+    if has_custom_opts then
+      opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
+    end
+    lspconfig[server_name].setup(opts)
+  end,
+}
