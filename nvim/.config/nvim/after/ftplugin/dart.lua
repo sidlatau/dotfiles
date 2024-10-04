25c619d61e5eb7750f94488a63e27335dc275de6
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2024-10-04 18:07:40 +0300

    vim: use lua syntax

--- nvim/.config/nvim/after/ftplugin/dart.lua
+++ nvim/.config/nvim/after/ftplugin/dart.lua
@@ -1,7 +1,4 @@
-vim.cmd [[
-setlocal fo+=r " Automatically insert comment string after hitting ENTER in insert mode
-
-setlocal iskeyword+=$
-]]
 vim.opt_local.spell = true
 vim.opt_local.spelloptions:append { "camel", "noplainbuffer" }
+vim.opt_local.iskeyword:append "$"
+vim.opt_local.fo:append "r" -- Automatically insert comment string after hitting ENTER in insert mode
