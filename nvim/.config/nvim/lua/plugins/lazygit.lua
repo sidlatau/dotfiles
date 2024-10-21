3e6461edd7518a10cc9447eb461017a638611e91
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2024-10-22 00:02:09 +0300

    vim: remove highlight when opening lazygit

--- nvim/.config/nvim/lua/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/plugins/lazygit.lua
@@ -11,7 +11,11 @@ return {
   -- setting the keybinding for LazyGit with 'keys' is recommended in
   -- order to load the plugin when the command is run for the first time
   keys = {
-    { "<leader>gg", "<cmd>wa<cr><cmd>LazyGit<cr>", desc = "LazyGit" },
+    {
+      "<leader>gg",
+      "<cmd>wa<cr><cmd>nohlsearch<cr><cmd>LazyGit<cr>",
+      desc = "LazyGit",
+    },
   },
   config = function()
     vim.g.lazygit_floating_window_scaling_factor = 1
