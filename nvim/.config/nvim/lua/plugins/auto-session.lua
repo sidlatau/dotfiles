80e2e3efb0808c7b7d4eb39249aad871653b3fa6
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2024-11-02 19:43:59 +0100

    feat: do not save log buffers in autosession

--- nvim/.config/nvim/lua/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -5,6 +5,7 @@ return {
     require("auto-session").setup {
       log_level = "error",
       lazy_support = true,
+      bypass_save_filetypes = { "log" },
       auto_session_suppress_dirs = {
         "~/",
         "~/Documents/github",
