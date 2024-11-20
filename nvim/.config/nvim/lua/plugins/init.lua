543a432f64135d8f9b22d2af72b49a916fab96fa
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2024-11-20 18:04:56 +0200

    vim: setup dotnet debugging

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -112,4 +112,11 @@ return {
     ft = "cs",
     opts = {},
   },
+  {
+    "Cliffback/netcoredbg-macOS-arm64.nvim",
+    dependencies = { "mfussenegger/nvim-dap" },
+    config = function()
+      require("netcoredbg-macOS-arm64").setup(require "dap")
+    end,
+  },
 }
