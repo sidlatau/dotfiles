37ceb5999ced5ae786a9461577fa331cb813395d
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2024-12-04 13:12:09 +0200

    feat: add grug-far.nvim plugin for search and replace

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -124,4 +124,14 @@ return {
       require("netcoredbg-macOS-arm64").setup(require "dap")
     end,
   },
+  {
+    "MagicDuck/grug-far.nvim",
+    config = function()
+      require("grug-far").setup {
+        -- options, see Configuration section below
+        -- there are no required options atm
+        -- engine = 'ripgrep' is default, but 'astgrep' can be specified
+      }
+    end,
+  },
 }
