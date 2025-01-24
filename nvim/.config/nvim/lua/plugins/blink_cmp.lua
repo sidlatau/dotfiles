db80a93aa4ec3fe793a5c4651dda9a268128fd08
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2025-01-24 23:37:32 +0200

    fix: update blink mappings

--- nvim/.config/nvim/lua/plugins/blink_cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink_cmp.lua
@@ -19,7 +19,7 @@ return {
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- see the "default configuration" section below for full documentation on how to define
     -- your own keymap.
-    keymap = { preset = "enter" },
+    keymap = { preset = "enter", ["<TAB>"] = {}, ["<C-k>"] = {} },
 
     appearance = {
       -- Sets the fallback highlight groups to nvim-cmp's highlight groups
