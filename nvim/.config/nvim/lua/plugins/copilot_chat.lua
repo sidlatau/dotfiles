2c26b9b67187994bfa85d98f9ae0aa9a8396cd57
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2025-03-12 23:22:23 +0200

    chore: update dependencies and refine CopilotChat config

--- nvim/.config/nvim/lua/plugins/copilot_chat.lua
+++ nvim/.config/nvim/lua/plugins/copilot_chat.lua
@@ -9,16 +9,10 @@ return {
     build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
       model = "claude-3.7-sonnet",
+      references_display = "write",
       highlight_headers = false,
       separator = "---",
       error_header = "> [!ERROR] Error",
-      -- callback = function(result)
-      --   local lines = vim.split(result, "\n")
-      --   table.remove(lines, 1) -- Remove the first line
-      --   table.remove(lines) -- Remove the last line
-      --   local new_result = table.concat(lines, "\n")
-      --   vim.fn.setreg("+", new_result) -- Copy to system clipboard
-      -- end,
       mappings = {
         complete = {
           insert = "<M-CR>",
@@ -28,13 +22,16 @@ return {
           full_diff = true, -- Show full diff instead of unified diff when showing diff window
         },
       },
+      sticky = {
+        "#files",
+      },
       prompts = {
         TranslateArb = {
           prompt = "Translate to language, specified in file name, translation is used in Flutter app, arb file. Do not add any explanatory comments. Include lines that are already translated, for ability to safely replace output.",
           description = "Translate arb file",
         },
         Commit = {
-          prompt = "> #git:staged\n\nWrite commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. List message code as list - each sentence in new line. Be concise in explanation. Wrap the whole message in code block with language gitcommit. Use branch name as scope if branch name contains digits (i.e `ID-123`, `DNA-123`).",
+          prompt = "> #git:staged\n\nWrite commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. List message code as list - each sentence in new line. Be concise in explanation, do not explain obvious things, do not repeat same information as in message header. Less is better. Wrap the whole message in code block with language gitcommit. Use branch name as scope if branch name contains digits (i.e `ID-123`, `DNA-123`).",
         },
         ReviewStaged = {
           prompt = "> /COPILOT_REVIEW\n\n#git:staged\n\n Review the staged code. Show link to file and line number. If there are multiple lines, show the range. If there are multiple files, show the file name.",
@@ -111,9 +108,6 @@ return {
         end,
         desc = "CopilotChat - Prompt actions",
       },
-    },
-    sticky = {
-      "#files",
     },
   },
 }
