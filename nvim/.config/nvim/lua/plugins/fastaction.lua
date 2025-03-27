e421390bbd7be0ebb3c21855791b20998be41c9a
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2025-03-27 20:06:19 +0200

    feat(plugins): replace clear-action with fastaction.nvim
    - Removed clear-action.nvim plugin and configuration
    - Added fastaction.nvim with similar keybindings and Dart-specific
      configurations for code actions
    - Updated related entries in lazy-lock.json

---
+++ nvim/.config/nvim/lua/plugins/fastaction.lua
@@ -0,0 +1,26 @@
+return {
+  "Chaitanyabsprip/fastaction.nvim",
+  ---@type FastActionConfig
+  opts = {
+    dismiss_keys = { "j", "k", "<c-c>", "<esc>" },
+    priority = {
+      dart = {
+        { pattern = "wrap with widget", key = "w", order = 1 },
+        { pattern = "padding", key = "p", order = 2 },
+        { pattern = "column", key = "c", order = 3 },
+        { pattern = "row", key = "r", order = 3 },
+        { pattern = "center", key = "C", order = 4 },
+        { pattern = "remove this widget", key = "R", order = 5 },
+        { pattern = "convert to block body", key = "c", order = 6 },
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>a",
+      '<cmd>lua require("fastaction").code_action()<CR>',
+      desc = "Lsp code action",
+      mode = { "n", "x" },
+    },
+  },
+}
