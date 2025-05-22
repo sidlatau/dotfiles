05ba224e6ad8d8505e1e82bd1195489110002521
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2025-05-22 22:59:03 +0300

    feat: update aider plugin config and keymaps, bump plugin lock versions

--- nvim/.config/nvim/lua/plugins/aider.lua
+++ nvim/.config/nvim/lua/plugins/aider.lua
@@ -1,21 +1,40 @@
 return {
   "GeorgesAlkhouri/nvim-aider",
   cmd = "Aider",
-  -- Example key mappings for common actions:
+  opts = {
+    auto_reload = true,
+    picker_cfg = {
+      preset = "select",
+    },
+  },
   keys = {
-    { "<leader>AA", "<cmd>Aider toggle<cr>", desc = "Toggle Aider" },
+    { "<leader><tab><tab>", "<cmd>Aider toggle<cr>", desc = "Toggle Aider" },
     {
-      "<leader>As",
+      "<leader><tab>s",
       "<cmd>Aider send<cr>",
       desc = "Send to Aider",
       mode = { "n", "v" },
     },
-    { "<leader>Ac", "<cmd>Aider command<cr>", desc = "Aider Commands" },
-    { "<leader>Ab", "<cmd>Aider buffer<cr>", desc = "Send Buffer" },
-    { "<leader>A+", "<cmd>Aider add<cr>", desc = "Add File" },
-    { "<leader>A-", "<cmd>Aider drop<cr>", desc = "Drop File" },
-    { "<leader>Ar", "<cmd>Aider add readonly<cr>", desc = "Add Read-Only" },
-    { "<leader>AR", "<cmd>Aider reset<cr>", desc = "Reset Session" },
+    { "<leader><tab>c", "<cmd>Aider command<cr>", desc = "Aider Commands" },
+    { "<leader><tab>b", "<cmd>Aider buffer<cr>", desc = "Send Buffer" },
+    { "<leader><tab>+", "<cmd>Aider add<cr>", desc = "Add File" },
+    { "<leader><tab>-", "<cmd>Aider drop<cr>", desc = "Drop File" },
+    { "<leader><tab>r", "<cmd>Aider add readonly<cr>", desc = "Add Read-Only" },
+    { "<leader><tab>R", "<cmd>Aider reset<cr>", desc = "Reset Session" },
+    {
+      "<leader><tab>a",
+      function()
+        local api = require("nvim_aider").api
+        vim.ui.input({ prompt = "Ask (empty to skip):" }, function(input)
+          if input ~= nil then
+            if input ~= "" then
+              api.send_command("/ask", input)
+            end
+          end
+        end)
+      end,
+      desc = "Aider ask",
+    },
   },
   dependencies = {
     "folke/snacks.nvim",
