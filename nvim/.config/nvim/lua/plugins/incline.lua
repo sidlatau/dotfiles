d281fdc338fd5af8fd0fa8f7a36cde376c0bda86
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2025-08-22 20:47:33 +0300

    feat(incline): add diagnostic label to bufferline display

--- nvim/.config/nvim/lua/plugins/incline.lua
+++ nvim/.config/nvim/lua/plugins/incline.lua
@@ -14,8 +14,33 @@ return {
           filename = "[No Name]"
         end
         local ft_icon, ft_color = devicons.get_icon_color(filename)
+
+        local function get_diagnostic_label()
+          local icons =
+            { error = "", warn = "", info = "", hint = "" }
+          local label = {}
+
+          for severity, icon in pairs(icons) do
+            local n = #vim.diagnostic.get(
+              props.buf,
+              { severity = vim.diagnostic.severity[string.upper(severity)] }
+            )
+            if n > 0 then
+              table.insert(
+                label,
+                { icon .. n .. " ", group = "DiagnosticSign" .. severity }
+              )
+            end
+          end
+          if #label > 0 then
+            table.insert(label, { "┊ " })
+          end
+          return label
+        end
         local modified = vim.bo[props.buf].modified
+
         return {
+          { get_diagnostic_label() },
           ft_icon and {
             modified and " ●" or "",
             " ",
